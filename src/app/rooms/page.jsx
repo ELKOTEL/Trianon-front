ece91fc95fd709998c9fe7fce71367404460a550
Elkotel Rabeb <elkotelrabeb111@gmail.com>
2025-03-09 14:59:38 +0100

    23th commit

--- src/app/rooms/page.jsx
+++ src/app/rooms/page.jsx
@@ -45,26 +45,77 @@ export default function Rooms() {
           <p className="text-lg max-w-2xl mx-auto text-white">
             Discover our luxurious rooms designed for the ultimate relaxation. Each room is equipped with premium amenities to make your stay unforgettable.
           </p>
+          
         </div>
 
         {/* Rooms Section */}
-        <div>
+        <div className="bg-white flex flex-col gap-6">
           {rooms.map((room, index) => (
             <div
               key={room._id}
               className={`flex flex-col gap-8 py-12 px-20 ${index % 2 === 0 ? "bg-white" : "bg-white"}`}
             >
               {/* Room Image Section (2 Images on Top) */}
-              <div className="flex mb-6 relative">
+              <div className="flex mb-6 relative ">
                 {/* Left Rectangule (1/3 width) */}
                 <div className={`w-1/3 h-[80vh] pt-32 ${index % 2 === 0 ? "" : "order-last"}`}>
-                  <div className={`bg-amber-600 h-full flex items-end pl-4 pb-4 ${index % 2 === 0 ? "pl-4" : "pr-4 justify-end"}`}>
-                    <h1 className="text-[#F4E1D2] text-1 font-serif tracking-wider">Trianon Hotel</h1>
+                  <div className={`bg-amber-600 h-full flex flex-col items-center justify-end pb-4  ${index % 2 === 0 ? " rounded-tl  rounded-bl" : "rounded-tr  rounded-br"}`}>
+                    <h1 className="text-[#F4E1D2] text-2xl font-serif tracking-wider">{room.name}</h1>
+                                      {/* Room Info Icons (Persons, Bed Type, Surface) */}
+                  <div className="flex gap-4 items-center text-lg pl-10 text-[#C79D77] ">
+                    <div className="flex items-center gap-2">
+                      <FaUser />
+                      <span>{room.persons}</span>
+                    </div>
+                    <div className="flex items-center gap-2">
+                      <FaBed />
+                      <span>{room.bed}</span>
+                    </div>
+                    <div className="flex items-center gap-2">
+                      <FaRulerCombined />
+                      <span>{room.surface} sqm</span>
+                    </div>
+                  </div>
                   </div>
 
+              {/* Room Details Below Images */}
+              <div className="flex flex-col items-center text-center ">
+                {/* High-End Amenities Section */}
+                <div className="text-black px-4 ">
+                  <h4 className="text-xl font-semibold my-2">High-end Amenities</h4>
+                  <p className="text-md mb-4">{room.description}</p>
+
+                  {/* Amenities List (No background, minimalist design) */}
+                  <div className="grid grid-cols-2 gap-x-20 gap-y-4 ">
+                    {room.amenities &&
+                      room.amenities.slice(0, 4).map((amenity, index) => (
+                        <li key={index} className="text-sm text-start">{amenity}</li>
+                      ))}
+                    {showMore && (
+                      <div className="grid grid-cols-2 gap-x-20 gap-y-4">
+                        {room.amenities.slice(4).map((amenity, index) => (
+                          <li key={index} className="text-sm text-start">{amenity}</li>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+
+                  {/* See More Equipment Link */}
+                  {room.amenities && room.amenities.length > 4 && (
+                    <div>
+                      <button
+                        className="text-[#C79D77] underline"
+                        onClick={handleShowMore}
+                      >
+                        {showMore ? "Show Less" : "See More Equipment"}
+                      </button>
+                    </div>
+                  )}
+                </div>
+              </div>
+                            {/* end Room Details Below Images */}
                   {/* Overlay box with room description */}
-                  <div className={`absolute top-10 w-1/3 px-10 py-16 bg-white z-10 shadow-xl ${index % 2 === 0 ? "left-20" : "right-20"}`}>
-                    <h3 className="text-2xl font-semibold text-[#5C4033] mb-4">{room.name}</h3>
+                  <div className={`absolute top-10 w-1/3 px-10 py-16 bg-white z-10 shadow-xl ${index % 2 === 0 ? "left-20 rounded-bl" : "right-20 rounded-br"}`}>
                     <div className="pl-20">
                       <p className="text-lg text-[#5C4033] mb-6">
                         The Apple TV's cutting-edge technology and free Wi-Fi connection make these rooms ideal for business travelers.
@@ -112,7 +163,7 @@ export default function Rooms() {
                         <img
                           src={urlFor(img).width(800).url()}
                           alt={`Room ${room.name}`}
-                          className="w-full h-full object-cover   "
+                          className="w-full h-full object-cover rounded   "
                         />
                       </SwiperSlide>
                     ))
@@ -121,59 +172,10 @@ export default function Rooms() {
                   )}
                 </Swiper>
                 </div>
-                  {/* Room Info Icons (Persons, Bed Type, Surface) */}
-                  <div className="flex gap-4 items-center text-lg pl-10 text-[#C79D77] ">
-                    <div className="flex items-center gap-2">
-                      <FaUser />
-                      <span>{room.persons}</span>
-                    </div>
-                    <div className="flex items-center gap-2">
-                      <FaBed />
-                      <span>{room.bed}</span>
-                    </div>
-                    <div className="flex items-center gap-2">
-                      <FaRulerCombined />
-                      <span>{room.surface} sqm</span>
-                    </div>
-                  </div>
+
                 </div>
               </div>
 
-              {/* Room Details Below Images */}
-              <div className="flex flex-col items-center text-center">
-                {/* High-End Amenities Section */}
-                <div className="text-black">
-                  <h4 className="text-xl font-semibold mb-2">High-end Amenities</h4>
-                  <p className="text-lg mb-4">{room.description}</p>
-
-                  {/* Amenities List (No background, minimalist design) */}
-                  <div className="grid grid-cols-2 gap-x-20 gap-y-4">
-                    {room.amenities &&
-                      room.amenities.slice(0, 4).map((amenity, index) => (
-                        <li key={index} className="text-sm text-start">{amenity}</li>
-                      ))}
-                    {showMore && (
-                      <div className="grid grid-cols-2 gap-x-20 gap-y-4">
-                        {room.amenities.slice(4).map((amenity, index) => (
-                          <li key={index} className="text-sm text-start">{amenity}</li>
-                        ))}
-                      </div>
-                    )}
-                  </div>
-
-                  {/* See More Equipment Link */}
-                  {room.amenities && room.amenities.length > 4 && (
-                    <div>
-                      <button
-                        className="text-[#C79D77] underline"
-                        onClick={handleShowMore}
-                      >
-                        {showMore ? "Show Less" : "See More Equipment"}
-                      </button>
-                    </div>
-                  )}
-                </div>
-              </div>
             </div>
           ))}
         </div>
